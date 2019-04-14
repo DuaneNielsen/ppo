f89a4c3cc1696e82ca74985825a47cadc8144cf8
duanenielsen <duane.nielsen.rocks@gmail.com>
2019-04-14 03:48:10 +0000

    Fixing dockerfile

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 
 setup(name='ppo',
       version='0.0.1',
-      install_requires=['git+https://github.com/Kojoley/atari-py.git',
-                        'https://download.pytorch.org/whl/cu100/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl'
+      install_requires=['torch',
                         'gym[classic_control]>=0.2.3', 'gym[box2d]>=0.2.3', 'gym[atari]>=0.2.3',
                         'numpy',
                         'py3nvml',
