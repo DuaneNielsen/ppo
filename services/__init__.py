0ec6efb59a6244148ad2b52dac64f578295055c5
duanenielsen <duane.nielsen.rocks@gmail.com>
2019-06-15 04:54:54 +0000

    Started unit tests for services

---
+++ services/__init__.py
@@ -0,0 +1,4 @@
+from .trainer import Trainer
+from .tensorboard import TensorBoardListener
+from .gatherer import Gatherer
+from .coordinator import Coordinator
--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,7 +1,19 @@
-from services.server import Server
+from services import Trainer
 from messages import *
+from services.server import Server
 import redis
 import pytest
+from uuid import uuid4
+from threading import Thread
+
+
+class ServerThread(Thread):
+    def __init__(self, s):
+        super().__init__()
+        self.s = s
+
+    def run(self):
+        self.s.main()
 
 
 @pytest.fixture
@@ -18,5 +30,9 @@ class ListenTestServer(Server):
         print(msg)
 
 
+def test_trainer_exit(r):
+    ServerThread(Trainer()).start()
+    ServerThread(ListenTestServer(r)).start()
+    ExitMessage(server_uuid=uuid4()).send(r)
 
 
